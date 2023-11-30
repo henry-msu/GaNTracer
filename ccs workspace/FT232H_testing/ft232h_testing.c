50ac95be667cc5eb2afb3cda6c36efb4f1dab332
Henry Naguski <henry-school@nilsu.org>
2023-11-30 10:47:39 -0700

    update FT232H test ccs project

--- ccs workspace/FT232H_testing/ft232h_testing.c
+++ ccs workspace/FT232H_testing/ft232h_testing.c
@@ -1,63 +1,68 @@
-//*****************************************************************************
-//!   SPI slave talks to SPI master using 3-wire mode. Data is received
-//!   from master and data from slave is then transmitted back to master.
-//!   USCI RX ISR is used to handle communication, CPU normally in LPM4.
-//!   Prior to initial data exchange, master pulses slaves RST for complete
-//!   reset.
-//!
-//!   Use with eusci_spi_ex1_master code example.  If the slave is in
-//!   debug mode, the reset signal from the master will conflict with slave's
-//!   JTAG; to work around, use IAR's "Release JTAG on Go" on slave device.  If
-//!   breakpoints are set in slave RX ISR, master must stopped also to avoid
-//!   overrunning slave RXBUF.
-//!
-//!             Tested on MSP430FR2355
-//!                 -----------------
-//!            /|\ |                 |
-//!             |  |                 |
-//!    Master---+->|RST              |
-//!                |                 |
-//!                |             P1.2|-> Data Out (UCB0SIMO)
-//!                |                 |
-//!                |             P1.3|<- Data In (UCB0SOMI)
-//!                |                 |
-//!                |             P1.1|<- Serial Clock In (UCB0CLK)
-//!
-//! This example uses the following peripherals and I/O signals.  You must
-//! review these and change as needed for your own board:
-//! - SPI peripheral
-//! - GPIO Port peripheral (for SPI pins)
-//! - UCB0SIMO
-//! - UCB0SOMI
-//! - UCB0CLK
-//!
-//! This example uses the following interrupt handlers.  To use this example
-//! in your own application you must add these interrupt handlers to your
-//! vector table.
-//! - USCI_B0_VECTOR
-//!
-//
-//*****************************************************************************
+/*
+ * FT232H (or any SPI master) to MSP430 slave using 4-wire, CS active low mode
+ *
+ *  FT232H                   MSP430FR2355
+ * --------|               |----------------|
+ *       DO|->Data Out---->|P1.2 (UCB0SIMO) |
+ *         |               |                |
+ *       DI|->Data In----->|P1.3 (UCB0SOMI) |
+ *         |               |                |
+ *       SK|->Clock------->|P1.1 (UCB0CLK)  |
+ *         |               |                |
+ *       CS|->Chip select->|P1.0 (UCB0STE)  |
+ * --------|               |                |
+ *                         |     (MCLK) P3.0|--> MCLK output
+ *                         |----------------|
+ *
+ * This example uses the following peripherals and I/O signals:
+ * - CS module (to set MCLK frequency to 8 MHz
+ * - EUSCI_B0 module (SPI mode)
+ * - GPIO Port module (for SPI pins)
+ *   - UCB0STE
+ *   - UCB0CLK
+ *   - UCB0SIMO
+ *   - UCB0SOMI
+ *   - MCLK
+ *
+ * This example uses the following interrupt handlers:
+ * - USCI_B0_VECTOR
+ * - UNMI_VECTOR
+ *
+ */
 #include "driverlib.h"
+#include "Board.h"
 
-uint8_t transmitData = 0;
-uint8_t receiveData = 0;
+#define CS_MCLK_DESIRED_FREQUENCY_IN_KHZ 24000 // Target frequency for MCLK in kHz
+#define CS_MCLK_FLLREF_RATIO CS_MCLK_DESIRED_FREQUENCY_IN_KHZ/32.768 // MCLK/FLLRef Ratio
+#define DATA_SIZE 2048 // how many bytes to send/receive
+
+uint32_t clockValue = 0; // current Clock values
+uint16_t status; // status of Oscillator fault flags
+
+uint8_t receiveData[DATA_SIZE] = {0}; // SPI RX memory
+uint16_t i = 0; // basic iterator
+uint8_t instruction = 0; // buffer to store what SPI instruction is being executed
+uint8_t executing = 0; // status of SPI, are we waiting for a new instruction or executing one
 
 void main(void) {
-    //Stop watchdog timer
-    WDT_A_hold(WDT_A_BASE);
+    WDT_A_hold(WDT_A_BASE); // Stop watchdog timer
 
-    // Configure SPI pins
-    // Configure Pins for UCB0CLK, UCB0TXD/UCB0SIMO and UCB0RXD/UCB0SOMI
     /*
-    * Select Port 1
-    * Set Pin 1, Pin 2 and Pin 3 to input with function.
-    */
+     * Configure pins for UCB0STE, UCB0CLK, UCB0SIMO and UCB0SOMI
+     * Set P1.0:3 to input with their primary function.
+     */
     GPIO_setAsPeripheralModuleFunctionInputPin(
         GPIO_PORT_P1,
-        GPIO_PIN1 + GPIO_PIN2 + GPIO_PIN3,
+        GPIO_PIN0 + GPIO_PIN1 + GPIO_PIN2 + GPIO_PIN3,
         GPIO_PRIMARY_MODULE_FUNCTION
     );
+
+    // MCLK output to GPIO
+    GPIO_setAsPeripheralModuleFunctionOutputPin(
+        GPIO_PORT_MCLK,
+        GPIO_PIN_MCLK,
+        GPIO_FUNCTION_MCLK
+        );
 
     /*
      * Disable the GPIO power-on default high-impedance mode to activate
@@ -65,31 +70,48 @@ void main(void) {
      */
     PMM_unlockLPM5();
 
-    //Initialize slave to MSB first, inactive low clock polarity and 3 wire SPI
-    EUSCI_B_SPI_initSlaveParam param = {0};
-    param.msbFirst = EUSCI_B_SPI_MSB_FIRST;
-    param.clockPhase = EUSCI_B_SPI_PHASE_DATA_CAPTURED_ONFIRST_CHANGED_ON_NEXT;
-    param.clockPolarity = EUSCI_B_SPI_CLOCKPOLARITY_INACTIVITY_LOW;
-    param.spiMode = EUSCI_B_SPI_3PIN;
-    EUSCI_B_SPI_initSlave(EUSCI_B0_BASE, &param);
+    // If running at more than 8 MHz, need FRAM wait states
+    FRAMCtl_configureWaitStateControl(FRAMCTL_ACCESS_TIME_CYCLES_2);
 
-    EUSCI_B_SPI_enable(EUSCI_B0_BASE); //Enable SPI Module
+    //Set DCO FLL reference = REFO
+    CS_initClockSignal(CS_FLLREF, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    //Set ACLK = REFO
+    CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
 
-    EUSCI_B_SPI_clearInterrupt(EUSCI_B0_BASE,
-            EUSCI_B_SPI_RECEIVE_INTERRUPT
-    );
+    CS_initFLLParam csparam = {0}; // Create struct variable to store proper software trim values
+    // Set Ratio/Desired MCLK Frequency, initialize DCO, save trim values
+    CS_initFLLCalculateTrim(CS_MCLK_DESIRED_FREQUENCY_IN_KHZ, CS_MCLK_FLLREF_RATIO, &csparam);
+    CS_clearAllOscFlagsWithTimeout(1000); // Clear all OSC fault flags
 
-    EUSCI_B_SPI_enableInterrupt(EUSCI_B0_BASE,
-          EUSCI_B_SPI_RECEIVE_INTERRUPT // enable rx interrupt
-    );
+    /*
+     * Initialize EUSCI_B0
+     * SPI slave
+     * MSB first
+     * Active high clock polarity
+     * 4 wire SPI
+     * CS active low
+     */
+    EUSCI_B_SPI_initSlaveParam spiparam = {0};
+    spiparam.msbFirst = EUSCI_B_SPI_MSB_FIRST;
+    spiparam.clockPhase = EUSCI_B_SPI_PHASE_DATA_CAPTURED_ONFIRST_CHANGED_ON_NEXT;
+    spiparam.clockPolarity = EUSCI_B_SPI_CLOCKPOLARITY_INACTIVITY_LOW;
+    spiparam.spiMode = EUSCI_B_SPI_4PIN_UCxSTE_ACTIVE_LOW;
+    EUSCI_B_SPI_initSlave(EUSCI_B0_BASE, &spiparam);
+    EUSCI_B_SPI_enable(EUSCI_B0_BASE); // Enable SPI module
 
-    __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, enable interrupts
+    EUSCI_B_SPI_clearInterrupt(EUSCI_B0_BASE, EUSCI_B_SPI_RECEIVE_INTERRUPT);
+    EUSCI_B_SPI_enableInterrupt(EUSCI_B0_BASE, EUSCI_B_SPI_RECEIVE_INTERRUPT); // Enable RX interrupt
+
+    SFR_enableInterrupt(SFR_OSCILLATOR_FAULT_INTERRUPT); // Enable oscillator fault interrupt
+
+    // MAIN LOOP
+    while(1) {
+        __bis_SR_register(LPM0_bits + GIE); // Enter LPM0, enable interrupts
+    }
 }
 
 //******************************************************************************
-//
 // USCI_B0 interrupt vector service routine.
-//
 //******************************************************************************
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector=USCI_B0_VECTOR
@@ -97,22 +119,64 @@ __interrupt
 #elif defined(__GNUC__)
 __attribute__((interrupt(USCI_B0_VECTOR)))
 #endif
-void USCI_B0_ISR (void)
-{
-    switch(__even_in_range(UCB0IV, USCI_SPI_UCTXIFG))
-    {
+void USCI_B0_ISR (void) {
+    switch(__even_in_range(UCB0IV, USCI_SPI_UCTXIFG)) {
         case USCI_SPI_UCRXIFG: // UCRXIFG
-            while (!EUSCI_B_SPI_getInterruptStatus(EUSCI_B0_BASE, EUSCI_B_SPI_TRANSMIT_INTERRUPT)) {
-                // do nothing while txbuf is full
+            if(!executing) { // if not currently executing something, read in an instruction
+               instruction = EUSCI_B_SPI_receiveData(EUSCI_B0_BASE);
+               i = 0;
+               executing = 1;
+               if(instruction == 0x01) { // write data back to master
+                   i = 0;
+                   EUSCI_B_SPI_disableInterrupt(EUSCI_B0_BASE, EUSCI_B_SPI_RECEIVE_INTERRUPT); // disable RX interrupt
+                   EUSCI_B_SPI_clearInterrupt(EUSCI_B0_BASE, EUSCI_B_SPI_TRANSMIT_INTERRUPT);
+                   EUSCI_B_SPI_enableInterrupt(EUSCI_B0_BASE, EUSCI_B_SPI_TRANSMIT_INTERRUPT); // enable TX interrupt
+                   // place first byte in transmit buffer, need to load it during this instruction or else the transmission will be late by 1 byte
+                   EUSCI_B_SPI_transmitData(EUSCI_B0_BASE, receiveData[i++]);
+               }
             }
-            //Receive data from master
-            receiveData = EUSCI_B_SPI_receiveData(EUSCI_B0_BASE);
-            transmitData = receiveData ^ 0xFF;
-            // Transmit data back to master
-            EUSCI_B_SPI_transmitData(EUSCI_B0_BASE, transmitData);
-
+            else if(instruction == 0x00) { // read bytes from master
+                receiveData[i++] = EUSCI_B_SPI_receiveData(EUSCI_B0_BASE);
+                if(i >= DATA_SIZE) {
+                    i = 0;
+                    executing = 0;
+                    instruction = 0;
+                }
+            }
             break;
+        case USCI_SPI_UCTXIFG: // UCTXIFG
+            if(i <= DATA_SIZE - 1) {
+                EUSCI_B_SPI_transmitData(EUSCI_B0_BASE, receiveData[i++]);
+            }
+            else if(i == DATA_SIZE) {
+            	i++;
+            }
+            else if(i > DATA_SIZE) {
+                i = 0;
+                executing = 0;
+                instruction = 0;
+                EUSCI_B_SPI_disableInterrupt(EUSCI_B0_BASE, EUSCI_B_SPI_TRANSMIT_INTERRUPT); // disable TX interrupt
+                EUSCI_B_SPI_clearInterrupt(EUSCI_B0_BASE, EUSCI_B_SPI_RECEIVE_INTERRUPT);
+                EUSCI_B_SPI_enableInterrupt(EUSCI_B0_BASE, EUSCI_B_SPI_RECEIVE_INTERRUPT); // enable RX interrupt
+            }
         default:
             break;
     }
+}
+
+//******************************************************************************
+// NMI vector service routine.
+//******************************************************************************
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=UNMI_VECTOR
+__interrupt
+#elif defined(__GNUC__)
+__attribute__((interrupt(UNMI_VECTOR)))
+#endif
+void NMI_ISR(void) {
+  do {
+    // If it still can't clear the oscillator fault flags after the timeout,
+    // trap and wait here.
+    status = CS_clearAllOscFlagsWithTimeout(1000);
+  } while(status != 0);
 }
