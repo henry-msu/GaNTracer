8d2d1dcc4d6e5752c3ca7ee3d528634bee805e5a
Lance G <lancedgonzalez@gmail.com>
2024-03-08 11:39:30 -0700

    DAC 1 byte transfer

--- ccs workspace/DAC-SPI-Communication/DAC-SPI-Master.c
+++ ccs workspace/DAC-SPI-Communication/DAC-SPI-Master.c
@@ -13,61 +13,52 @@
 #include <msp430.h> 
 #include <stdint.h>
 
-#define DAC_CS BIT0				//CS is connected to P1.0
 
-void SPI_DAC_INIT(); 
-void SPI_transmit(); 
+// void SPI_DAC_INIT(); 
+// void SPI_transmit(); 
 
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 
-	// SPI_DAC_INIT();
+	
 
+    // Configure SPI
+		UCB0CTLW0 |= UCSWRST;  		// Put eUSCI_B in reset
+		
+		// Configure SPI pins
+			P1SEL0 |=  BIT2 | BIT1;  	//SIMO (P1.2), SCLK (P1.1)
+			P1SEL1 &= ~(BIT2 | BIT1);
+			P1DIR |= BIT0;   			// Set CS pin (P1.0) as output
+			P1OUT |= BIT0;   			// Set CS pin high initially
+
+		UCB0CTLW0 |= UCSYNC | UCMSB | UCMST | UCSSEL__SMCLK;  // 3-pin, 8-bit SPI master
+		UCB0BRW = 10;           	// Set SPI clock to SMCLK / 1
+		UCB0CTLW0 &= ~UCSWRST;  	// Release eUSCI_B from reset
+
+	//DCSweep Ilim - Enable PWM controller
 	P3DIR |= BIT7;
 	P3OUT |= BIT7;
 
+	//Unlock LPM5
 	PM5CTL0 &= ~LOCKLPM5;
 
-	//uint32_t data_frame = 0x123456;
+	int i;
 
 	while(1){
-		// SPI_transmit(data_frame);
-		// __bis_SR_register(LPM0_bits | GIE); // Enter Low Power Mode 0 with interrupts enabled
+		UCB0TXBUF = 0xB2;
+		__delay_cycles(1000); 
 	}
-	
 	return 0;
 }
 
-void SPI_DAC_INIT(void){
-	// Configure SPI pins
-		P1SEL0 |=  BIT2 | BIT1;  //SIMO (P1.2), SCLK (P1.1)
-		P1SEL1 &= ~(BIT2 | BIT1);
-
-    // Configure CS pin
-		P1DIR |= DAC_CS;   // Set CS pin (P1.0) as output
-		P1OUT |= DAC_CS;   // Set CS pin high initially
-
-    // Configure SPI
-		UCB0CTLW0 |= UCSWRST;  // Put eUSCI_B in reset
-		UCB0CTLW0 |= UCSYNC | UCCKPL | UCMSB | UCMST | UCSSEL__SMCLK;  // 3-pin, 8-bit SPI master
-		UCB0BRW = 10;        // Set SPI clock to SMCLK / 1
-		UCB0CTLW0 &= ~UCSWRST; // Release eUSCI_B from reset
-}
-
-void SPI_transmit(uint32_t data) {
-    P1OUT &= ~DAC_CS;     // Select the DAC
-    UCB0TXBUF = (data >> 16) & 0xFF; // Send MSB first
-}
-
-#pragma vector = USCI_B0_VECTOR
-__interrupt void USCI_B0_ISR(void){
-	switch(__even_in_range(UCB0IV, USCI_SPI_UCTXIFG)){
-		case USCI_NONE: break;             // Vector 0: No interrupts
-        case USCI_SPI_UCTXIFG:             // Vector 2: TXIFG
-            P1OUT |= DAC_CS;               // Deselect the DAC
-            __bic_SR_register_on_exit(LPM0_bits); // Exit Low Power Mode 0
-            break;
-        default: break;
-	}
-}
+// #pragma vector = USCI_B0_VECTOR
+// __interrupt void USCI_B0_ISR(void){
+// 	switch(__even_in_range(UCB0IV, USCI_SPI_UCTXIFG)){
+// 		case USCI_NONE: break;             // Vector 0: No interrupts
+//         case USCI_SPI_UCTXIFG:             // Vector 2: TXIFG
+//             P1OUT |= DAC_CS;               // Deselect the DAC
+//             break;
+//         default: break;
+// 	}
+// }
