67bcf5df6dc0c0bc833968173570c90720c88263
Lance G <lancedgonzalez@gmail.com>
2024-03-25 09:34:47 -0600

    Multibyte SPI w/o enable control

--- ccs workspace/DAC-SPI-Communication/DAC-SPI-Master.c
+++ ccs workspace/DAC-SPI-Communication/DAC-SPI-Master.c
@@ -11,100 +11,101 @@
 *----------------------------------------------------------------------------*/
 
 #include <msp430.h> 
-#include <stdint.h>
 
-#define DAC_CS BIT0 // Define DAC chip select pin as BIT0 (P1.0)
+char packet[] = {0xF0, 0xF0, 0xF0, 0x40};
+unsigned int position, i;
 
-//Define DAC Commands
-#define Com_Write2InRegN            0x00
-#define Com_UpdateDacReg            0x01
-#define Com_Write2InRegN_SW_LDAC    0x02
-#define Com_WriteUpdateDacRegN      0x03
-#define Com_PowerDownDac            0x04
-#define Com_ResetDac                0x05
-#define Com_LDAC_SetReg             0x06
-#define Com_DAC_intRef              0x07
-
-//Define DAC Address
-#define DAC_A_addr                  0x00
-#define DAC_B_addr                  0x01
-#define DAC_All_addr                0x07
-
-// #define DATA_LENGTH 10
-
-void SPI_Init(); 
-void DAC_Tx(); 
 
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 
-	SPI_Init(); 
+    //Initialize SPI 3 Wire
+		SPI_Init(); 
 
 	//DCSweep Ilim - Enable PWM controller
-	P3DIR |= BIT7;
-	P3OUT |= BIT7;
+        P3DIR |= BIT7;
+        P3OUT |= BIT7;
 
-	//Unlock LPM5
-	PM5CTL0 &= ~LOCKLPM5;
+	//IRQs enable
+		P4IFG &= ~BIT1; 
+		P4IE |= BIT1; 
 
-	//uint8_t data_to_send[DATA_LENGTH] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
+		UCB0IFG &= ~UCTXIFG;
+		UCB0IE |= UCTXIE; 
 
-	int i;
+		__enable_interrupt(); 
 
-	while(1){
-		 // Send each byte in the data array
-    	for (i = 0; i < DATA_LENGTH; i++) {
-        	DAC_Tx(command, address, data)
-    	}
-	__delay_cycles(1000);
-	}
+	while(1){}
 	return 0;
 }
 
-// SPI-Initialize the SPI module for communication with the DAC
+//SPI-Initialize the SPI module for communication with the DAC
 void SPI_Init(void){
-	// Configure GPIOs for SPI mode
-    	P1SEL0 |= BIT1 | BIT2 | BIT3 | BIT0; // Set pins for SPI: P1.1 (CLK), P1.2 (MOSI), P1.3 (MISO), P1.0 (CS)
-    	P1SEL1 &= ~(BIT1 | BIT2 | BIT3 | BIT0);
 
     // Configure eUSCI_B0 for SPI
-		UCB0CTLW0 |= UCSWRST;               // Put eUSCI_B0 in reset
-		UCB0CTLW0 |= UCSYNC | UCCKPH | UCMSB | UCMST | UCSSEL__SMCLK;  // 4-wire SPI, Clock phase = 1, MSB first, Master mode, SMCLK as clock source
-		UCB0BRW = 0x0A;                     // Set SPI baud rate (frequencies)
-		UCB0CTLW0 &= ~UCSWRST;              // Release eUSCI_B0 for operation
+        UCB0CTLW0 |= UCSWRST;               // Put eUSCI_B0 in reset
+        UCB0CTLW0 |= (UCSSEL__SMCLK | UCSYNC | UCMSB | UCMST);           //1MHz clock, Synchronus, 
+        UCB0BRW = 10;                     	// Set SPI baud rate ( 10kHz )
+
+    // Configure GPIOs for SPI mode
+        P4DIR &= ~BIT1;
+        P4REN |= BIT1;
+        P4OUT |= BIT1;
+        P4IES |= BIT1;
+
+        P1SEL0 |= BIT0;
+        P1SEL1 &= ~BIT0;
+
+        P1SEL0 |= BIT1;
+        P1SEL1 &= ~BIT1;
+
+        P1SEL0 |= BIT2;
+        P1SEL1 &= ~BIT2;
+
+        PM5CTL0 &= ~LOCKLPM5;				 //Unlock LPM5
+
+        UCB0CTLW0 &= ~UCSWRST;              // Release eUSCI_B0 for operation
 }
 
-void DAC_Tx(uint16_t command, uint16_t address,  uint16_t data){
-	//---Data Frame:-------------------------------------------------------------
-	//| 23 | 22 | 21 | 20 | 19 | 18 | 17 | 16 | 15 | 14 | 13 | 12 | 11 | 10 | 09 | 08 | 07 | 06 | 05 | 04 | 03 | 02 | 01 | 01
-	//----------------------------------------------------------------
-	//| -  | -  | C2 | C1 | C0 | A2 | A1 | A0 | D11| D10| D9 | D8 | D7 | D6 | D5 | D4 | D3 | D2 | D1 | D0 | -  | -  | -  | -  |
-	//---------------------------------------------------------------------------
+// void DAC_Tx(uint16_t command, uint16_t address,  uint16_t data){
+// 	//---Data Frame:-------------------------------------------------------------
+// 	//| 23 | 22 | 21 | 20 | 19 | 18 | 17 | 16 | 15 | 14 | 13 | 12 | 11 | 10 | 09 | 08 | 07 | 06 | 05 | 04 | 03 | 02 | 01 | 01
+// 	//----------------------------------------------------------------
+// 	//| -  | -  | C2 | C1 | C0 | A2 | A1 | A0 | D11| D10| D9 | D8 | D7 | D6 | D5 | D4 | D3 | D2 | D1 | D0 | -  | -  | -  | -  |
+// 	//---------------------------------------------------------------------------
 
-	//Create a data frame that contains command, address, and data in proper spots
-	uint32_t data_frame = ((command & 0x07) << 20 | (address & 0x07) << 18 | (data & 0x0FFF) << 4);
+// 	//Create a data frame that contains command, address, and data in proper spots
+// 	uint32_t data_frame = ((command & 0x07) << 20 | (address & 0x07) << 18 | (data & 0x0FFF) << 4);
 
-	//Send 3 8bit SPI transmissions to the DAC containing the reduced data frame bits from DB23 - DB0
-	// Transmit the data frame
-    P1OUT &= ~DAC_CS;     // Select the DAC
-    UCB0TXBUF = (data_frame >> 16) & 0xFF; // Send MSB first
-    while (!(UCB0IFG & UCTXIFG)); // Wait for TX buffer to be ready
-    UCB0TXBUF = (data_frame >> 8) & 0xFF;
-    while (!(UCB0IFG & UCTXIFG)); // Wait for TX buffer to be ready
-    UCB0TXBUF = data_frame & 0xFF;
-    while (!(UCB0IFG & UCTXIFG)); // Wait for TX buffer to be ready
-    P1OUT |= DAC_CS; // Deselect the DAC
+// 	//Send 3 8bit SPI transmissions to the DAC containing the reduced data frame bits from DB23 - DB0
+// 	// Transmit the data frame
+//     P1OUT &= ~DAC_CS;     // Select the DAC
+//     UCB0TXBUF = (data_frame >> 16) & 0xFF; // Send MSB first
+//     while (!(UCB0IFG & UCTXIFG)); // Wait for TX buffer to be ready
+//     UCB0TXBUF = (data_frame >> 8) & 0xFF;
+//     while (!(UCB0IFG & UCTXIFG)); // Wait for TX buffer to be ready
+//     UCB0TXBUF = data_frame & 0xFF;
+//     while (!(UCB0IFG & UCTXIFG)); // Wait for TX buffer to be ready
+//     P1OUT |= DAC_CS; // Deselect the DAC
 	
+// }
+
+#pragma vector = PORT4_VECTOR
+__interrupt void ISR_PORT4_S1(void){
+	position = 0; 
+	UCB0TXBUF = packet[position];
+	P4IFG &= ~BIT1; 
 }
 
-// #pragma vector = USCI_B0_VECTOR
-// __interrupt void USCI_B0_ISR(void){
-// 	switch(__even_in_range(UCB0IV, USCI_SPI_UCTXIFG)){
-// 		case USCI_NONE: break;             // Vector 0: No interrupts
-//         case USCI_SPI_UCTXIFG:             // Vector 2: TXIFG
-//             P1OUT |= DAC_CS;               // Deselect the DAC
-//             break;
-//         default: break;
-// 	}
-// }
+
+#pragma vector = USCI_B0_VECTOR
+__interrupt void USCI_B0_ISR(void){
+	position++; 
+	if (position < sizeof(packet)){
+		UCB0TXBUF = packet[position];
+	}
+	else{
+		UCB0IFG &= ~UCTXIFG; 
+	}
+}
