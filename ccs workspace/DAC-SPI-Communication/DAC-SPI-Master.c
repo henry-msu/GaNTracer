e69fd7f731b35e7d1bb35648e800b8bda203861e
Lance G <lancedgonzalez@gmail.com>
2024-03-25 10:54:19 -0600

    "Added macros, global variables, and functions for SPI communication with DAC. Modified SPI initialization and DAC transmission functions. Removed IRQ enables and interrupt service routines."

--- ccs workspace/DAC-SPI-Communication/DAC-SPI-Master.c
+++ ccs workspace/DAC-SPI-Communication/DAC-SPI-Master.c
@@ -11,11 +11,36 @@
 *----------------------------------------------------------------------------*/
 
 #include <msp430.h> 
+#include <stdint.h>
 
-char packet[] = {0xF0, 0xF0, 0xF0, 0x40};
-unsigned int position, i;
+//----- Macro Definitions ----------------------------------------------------/
+#define W2InRegN 		0b000
+#define updateDACRegN 	0b000
 
+#define AddrDacA		0b000
+#define AddrDacB		0b001
+#define AddrDacAll 		0b111
+//----------------------------------------------------------------------------/
 
+//----- Global Variable Declarations------------------------------------------/
+char dataframe[];
+unsigned int i, j;
+
+typedef struct{
+	uint8_t SPI1; 
+	uint8_t	SPI2; 
+	uint8_t SPI3; 
+} FrameParts;
+FrameParts SPI_Frames;
+//------------------------------------------------------------------------------/
+
+//----- Function Declarations --------------------------------------------------/
+void SPI_Init(); 
+void DAC_SetTx(uint8_t, uint8_t, uint16_t);
+void Send2DAC(); 
+//------------------------------------------------------------------------------/
+
+/* MAIN PROGRAM */
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
@@ -23,20 +48,21 @@ int main(void)
     //Initialize SPI 3 Wire
 		SPI_Init(); 
 
+	//Initialize Frame Parts Structure
+		SPI_Frames.SPI1 = 0x00; 
+		SPI_Frames.SPI2 = 0x00; 
+		SPI_Frames.SPI3 = 0x00; 
+
 	//DCSweep Ilim - Enable PWM controller
         P3DIR |= BIT7;
         P3OUT |= BIT7;
 
-	//IRQs enable
-		P4IFG &= ~BIT1; 
-		P4IE |= BIT1; 
+	while(1){
+		DAC_SetTx(W2InRegN, AddrDacA, 0b00110101); 
+		Send2DAC();
+		for(j=0; j<1000; j++){}
+	}
 
-		UCB0IFG &= ~UCTXIFG;
-		UCB0IE |= UCTXIE; 
-
-		__enable_interrupt(); 
-
-	while(1){}
 	return 0;
 }
 
@@ -47,6 +73,9 @@ void SPI_Init(void){
         UCB0CTLW0 |= UCSWRST;               // Put eUSCI_B0 in reset
         UCB0CTLW0 |= (UCSSEL__SMCLK | UCSYNC | UCMSB | UCMST);           //1MHz clock, Synchronus, 
         UCB0BRW = 10;                     	// Set SPI baud rate ( 10kHz )
+		UCB0CTLW0 |= UCMODE1; 
+		UCB0CTLW0 &= ~UCMODE0; 
+		UCB0CTLW0 |= UCSTEM; 
 
     // Configure GPIOs for SPI mode
         P4DIR &= ~BIT1;
@@ -68,44 +97,53 @@ void SPI_Init(void){
         UCB0CTLW0 &= ~UCSWRST;              // Release eUSCI_B0 for operation
 }
 
-// void DAC_Tx(uint16_t command, uint16_t address,  uint16_t data){
-// 	//---Data Frame:-------------------------------------------------------------
-// 	//| 23 | 22 | 21 | 20 | 19 | 18 | 17 | 16 | 15 | 14 | 13 | 12 | 11 | 10 | 09 | 08 | 07 | 06 | 05 | 04 | 03 | 02 | 01 | 01
-// 	//----------------------------------------------------------------
-// 	//| -  | -  | C2 | C1 | C0 | A2 | A1 | A0 | D11| D10| D9 | D8 | D7 | D6 | D5 | D4 | D3 | D2 | D1 | D0 | -  | -  | -  | -  |
-// 	//---------------------------------------------------------------------------
+void DAC_SetTx(uint8_t command, uint8_t address,  uint16_t data){
+	//---Data Frame:-------------------------------------------------------------
+	//| 23 | 22 | 21 | 20 | 19 | 18 | 17 | 16 | 15 | 14 | 13 | 12 | 11 | 10 | 09 | 08 | 07 | 06 | 05 | 04 | 03 | 02 | 01 | 01
+	//----------------------------------------------------------------
+	//| -  | -  | C2 | C1 | C0 | A2 | A1 | A0 | D11| D10| D9 | D8 | D7 | D6 | D5 | D4 | D3 | D2 | D1 | D0 | -  | -  | -  | -  |
+	//---------------------------------------------------------------------------
 
-// 	//Create a data frame that contains command, address, and data in proper spots
-// 	uint32_t data_frame = ((command & 0x07) << 20 | (address & 0x07) << 18 | (data & 0x0FFF) << 4);
+	//Compile the command, address, and data  into a single variable.
+		uint32_t dc_mask = 0b00 << 23 | 0b0000 << 0;
+		uint32_t TXData = (command << 21) | (address << 18) | (data & 0b111111111111) << 3 | dc_mask;
+		
+	// Extract the 3, 2-byte items from the TXData variable
+		SPI_Frames.SPI1 = (TXData >> 16) & 0xFFFF;
+		SPI_Frames.SPI2 = (TXData >> 8) & 0xFF;
+		SPI_Frames.SPI3 = TXData & 0xFF;
+}
 
-// 	//Send 3 8bit SPI transmissions to the DAC containing the reduced data frame bits from DB23 - DB0
-// 	// Transmit the data frame
-//     P1OUT &= ~DAC_CS;     // Select the DAC
-//     UCB0TXBUF = (data_frame >> 16) & 0xFF; // Send MSB first
-//     while (!(UCB0IFG & UCTXIFG)); // Wait for TX buffer to be ready
-//     UCB0TXBUF = (data_frame >> 8) & 0xFF;
-//     while (!(UCB0IFG & UCTXIFG)); // Wait for TX buffer to be ready
-//     UCB0TXBUF = data_frame & 0xFF;
-//     while (!(UCB0IFG & UCTXIFG)); // Wait for TX buffer to be ready
-//     P1OUT |= DAC_CS; // Deselect the DAC
-	
+void Send2DAC(void){
+    // Pointer to the beginning of the SPI_Frames structure
+    uint8_t *ptr = (uint8_t *)&SPI_Frames;
+    
+    // Iterate through each byte of the structure
+    for (i = 0; i < sizeof(SPI_Frames); i++){
+        // Send the current byte through SPI
+        UCB0TXBUF = *(ptr + i);
+        
+        // Wait for transmission to complete
+        while (!(UCB0IFG & UCTXIFG));
+    }
+}
+
+// #pragma vector = PORT4_VECTOR
+// __interrupt void ISR_PORT4_S1(void){
+// //	P1OUT &= ~BIT0;
+// //	position = 0;
+// //	UCB0TXBUF = [position];
+// 	P4IFG &= ~BIT1; 
 // }
 
-#pragma vector = PORT4_VECTOR
-__interrupt void ISR_PORT4_S1(void){
-	position = 0; 
-	UCB0TXBUF = packet[position];
-	P4IFG &= ~BIT1; 
-}
 
-
-#pragma vector = USCI_B0_VECTOR
-__interrupt void USCI_B0_ISR(void){
-	position++; 
-	if (position < sizeof(packet)){
-		UCB0TXBUF = packet[position];
-	}
-	else{
-		UCB0IFG &= ~UCTXIFG; 
-	}
-}
+// #pragma vector = USCI_B0_VECTOR
+// __interrupt void USCI_B0_ISR(void){
+// //	position++;
+// //	if (position < sizeof(dataframe)){
+// //		UCB0TXBUF = packet[position];
+// //	}
+// //	else{
+// 		UCB0IFG &= ~UCTXIFG; 
+// //	}
+// }
